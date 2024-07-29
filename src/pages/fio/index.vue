25478b579850096be3abd861be041ffb7aace01d
xcondor <xcondor@gmail.com>
2024-07-29 16:27:01 +0800

    update

---
+++ src/pages/fio/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{$t('common').abnormal}}- {{time}}</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: this.$t("common").inProcess,
+      msg: this.$t("common").Free,
+      time: performance.now()
+    };
+  }
+};
+</script>
